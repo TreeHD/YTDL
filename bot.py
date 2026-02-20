375ba9def233f694906cfa7a9618b7d21db2a8ed
TreexHD <me@treexhd.me>
2026-02-20 20:08:44 +0800

    Configure httpx logger to WARNING level.

--- bot.py
+++ bot.py
@@ -31,6 +31,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 # Global queues
 request_queue = asyncio.Queue()
